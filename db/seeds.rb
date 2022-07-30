cfc1f2112e5c4d02fe54601ae4f9ff39037566be
Samantha Yeang <sammyyeang@gmail.com>
2022-07-30 14:47:56 +0800

    completed resto-review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning Database..."
+Restaurant.destroy_all
+
+category_array = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Creating 5 Restaurants..."
+5.times do |r|
+  name = Faker::Restaurant.name
+  category = category_array.sample
+  address = Faker::Address.full_address
+  content = Faker::Restaurant.review
+  rating = rand(0..5)
+
+  restaurant = Restaurant.new(name: name, address: address, category: category)
+  restaurant.save
+  review = Review.new(content: content, rating: rating)
+  review.restaurant = restaurant
+  review.save
+  puts "#{restaurant.name} is created"
+end
